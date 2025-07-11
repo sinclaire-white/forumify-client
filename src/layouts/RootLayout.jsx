381c98c485ffcf29874da840004f512a1404a07b
sinclaire-white <shahriyarsifat2@gmail.com>
2025-07-12 03:28:10 +0600

    project initiated

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
+
+import Footer from "../components/Footer";
+
+
+const RootLayout = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default RootLayout;
---
+++ src/router/router.jsx
@@ -0,0 +1,22 @@
+import {
+  createBrowserRouter,
+} from "react-router";
+import RootLayout from "../layouts/RootLayout";
+import Home from "../pages/home/Home";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children:[
+        {
+            index: true,
+            Component: Home
+        }
+
+    ]
+  },
+]);
+
+
